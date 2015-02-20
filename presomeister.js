7080b9e9e7a00693e7df3a2ad00a66dd2de2f352
Kippetje <karin.keijzer@gmail.com>
2015-02-20 02:11:42 +0100

    fix path

--- presomeister.js
+++ presomeister.js
@@ -1,7 +1,10 @@
 var dirPath = 'C:\Capathon\presomeister\preso-meister\FRONT';
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
+// RestURLs
 app.get('/', function(req, res){
   res.send('<h1>Hello</h1>');
 });
@@ -9,6 +12,11 @@ app.get('/presomeister/', function(req, res){
   res.sendFile('/FRONT/index.html', {"root": __dirname+'/..'});
 });
 
+app.use(express.static(__dirname + '/FRONT/'));
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
