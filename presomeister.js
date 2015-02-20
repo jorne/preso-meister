cd94099a567e6f2057f18c7840877d0ffbf7a741
Kippetje <karin.keijzer@gmail.com>
2015-02-20 17:19:40 +0100

    Updated port number to 80.

--- presomeister.js
+++ presomeister.js
@@ -87,6 +87,6 @@ io.on('connection', function(socket){
 });
 
 // Start server
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(80, function(){
+  console.log('listening on *:80');
 });
