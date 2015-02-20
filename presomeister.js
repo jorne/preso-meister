90473ac3726b24095b1d2771de33befb2fc7c3b7
Kippetje <karin.keijzer@gmail.com>
2015-02-20 02:45:00 +0100

    add chat

--- presomeister.js
+++ presomeister.js
@@ -19,8 +19,16 @@ app.use(express.static(__dirname + '/FRONT'));
 
 io.on('connection', function(socket){
   console.log('a user connected');
-});
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+
+});
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
