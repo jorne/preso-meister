675a15cb7d5f96a796e23d734262b171f662d7f8
Kippetje <karin.keijzer@gmail.com>
2015-02-20 15:09:36 +0100

    update the clear presentation name.

--- presomeister.js
+++ presomeister.js
@@ -37,9 +37,10 @@ app.get('/presentationName/', function(req, res){
 app.post('/presentationName/', checkAuth, function(req, res){
   if (req.query.presentationName) {
     presentationName = req.query.presentationName;
-	res.send('{\'ok\':\'Presentation selected: '+presentationName+'\'}');
+    res.send('{\'ok\':\'Presentation selected: '+presentationName+'\'}');
   } else {
-    res.send('error');
+    presentationName = "";
+    res.send('{\'ok\':\'No pesentation selected.'}');
   }
 });
 
